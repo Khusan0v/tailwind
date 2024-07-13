7d65010e6e74d6ffad93e7f09c71a9a96bcd9721
Ibrohim <atozbbss@gmail.com>
2024-07-13 19:55:00 +0500

    tailwind

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    
+    <div className='text-center	mt-10	'>
+      <div className='bg-slate-500 mx-auto	 w-20 h-20 rounded-full	'></div>
+      <h1 className="text-3xl  mb-10  ">
+      Headline
+      </h1>
+      <p className='text-slate-500 text-2xl'>Please add your content <br /> here. Keep it short and <br /> simple. And smile :)</p>
+      </div>
+    
+  )
+}
+
+export default App
